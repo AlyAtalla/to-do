4ed9d8660b339f614286bfaff863e8ada921a025
AlyAtalla <101482711+AlyAtalla@users.noreply.github.com>
2023-07-12 02:02:51 +0300

    Add assets, images, new js files, more styling

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,32 @@
-(()=>{const e=[{description:"Task 1",completed:!1,index:1},{description:"Task 2",completed:!0,index:2},{description:"Task 3",completed:!1,index:3}];window.addEventListener("load",(function(){const t=document.getElementById("todo-list");e.sort(((e,t)=>e.index-t.index)),e.forEach((e=>{const n=document.createElement("li");n.textContent=e.description,e.completed&&(n.style.textDecoration="line-through"),t.appendChild(n)}))}))})();
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+eval("\n\n//# sourceURL=webpack://to-do-list/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;
