91f2e738f417b33a138e931a481510822e566767
AlyAtalla <101482711+AlyAtalla@users.noreply.github.com>
2023-07-12 02:10:46 +0300

    Fix Eslint error

--- src/modules/interactive.js
+++ src/modules/interactive.js
@@ -1,6 +1,6 @@
 const toggle = (index, task) => {
   task.tasksArray.forEach((item) => {
-    if (item.index == index) {
+    if (item.index === index) {
       item.completed = !item.completed;
     }
   });
@@ -10,7 +10,7 @@ const toggle = (index, task) => {
 const clearAll = (task) => {
   task.tasksArray.forEach((data) => {
     if (data.completed === true) {
-      task.tasksArray = task.tasksArray.filter((item) => item.index != data.index);
+      task.tasksArray = task.tasksArray.filter((item) => item.index !== data.index);
     }
   });
   task.tasksArray.forEach((task1, i) => {
