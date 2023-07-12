268b1dc9fa30c1d30cfd50394e48c403525e08b5
AlyAtalla <101482711+AlyAtalla@users.noreply.github.com>
2023-07-12 11:24:26 +0300

    fix linter errors

--- src/modules/interactive.js
+++ src/modules/interactive.js
@@ -10,7 +10,7 @@ const toggle = (index, task) => {
 const clearAll = (task) => {
   const completedTasks = task.tasksArray.filter((data) => data.completed === true);
   task.tasksArray =
-    task.tasksArray.filter((item) => item.index !== completedTasks.map((data) => data.index));
+  task.tasksArray.filter((item) => item.index !== completedTasks.map((data) => data.index));
   completedTasks.forEach((task1) => {
     task1.index = undefined;
   });
