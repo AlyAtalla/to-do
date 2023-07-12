1935679d905b136831098e9fa73274763c53a7d9
AlyAtalla <101482711+AlyAtalla@users.noreply.github.com>
2023-07-12 12:40:27 +0300

    Fix Functionality in interactive.js

--- src/modules/interactive.js
+++ src/modules/interactive.js
@@ -1,6 +1,6 @@
 const toggle = (index, task) => {
   task.tasksArray.forEach((item) => {
-    if (item.index === index) {
+    if (item.index == index) {
       item.completed = !item.completed;
     }
   });
@@ -8,11 +8,13 @@ const toggle = (index, task) => {
 };
 
 const clearAll = (task) => {
-  const completedTasks = task.tasksArray.filter((data) => data.completed === true);
-  const completedTaskIndices = completedTasks.map((data) => data.index);
-  task.tasksArray = task.tasksArray.filter((item) => !completedTaskIndices.includes(item.index));
-  completedTasks.forEach((task1) => {
-    task1.index = undefined;
+  task.tasksArray.forEach((data) => {
+    if (data.completed === true) {
+      task.tasksArray = task.tasksArray.filter((item) => item.index != data.index);
+    }
+  });
+  task.tasksArray.forEach((task1, i) => {
+    task1.index = i;
   });
   task.addTask();
 };
