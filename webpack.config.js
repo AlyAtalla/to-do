48d6f227caccd9b46c5a2ab70560d8f1a0d24ba9
AlyAtalla <101482711+AlyAtalla@users.noreply.github.com>
2023-07-12 11:04:56 +0300

    Update webpack configuration, import js as module

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development', // Set the mode option to 'development' or 'production'
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
