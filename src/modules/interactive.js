1b0581397581c6988d35e0cd77e13295da086e4d
AlyAtalla <101482711+AlyAtalla@users.noreply.github.com>
2023-07-12 11:26:18 +0300

    fix linter error

--- src/modules/interactive.js
+++ src/modules/interactive.js
@@ -9,8 +9,7 @@ const toggle = (index, task) => {
 
 const clearAll = (task) => {
   const completedTasks = task.tasksArray.filter((data) => data.completed === true);
-  task.tasksArray =
-  task.tasksArray.filter((item) => item.index !== completedTasks.map((data) => data.index));
+  task.tasksArray = task.tasksArray.filter(item => item.index !== completedTasks.map(d => d.index));
   completedTasks.forEach((task1) => {
     task1.index = undefined;
   });
